40f9f0bef30fa52e7651990087dc255c62a857a4
Jyoti Lama <jyotilama@microsoft.com>
2025-04-24 12:35:43 -0700

    update az cli commands

--- BuildLab/create-devbox-onbehalf.ps1
+++ BuildLab/create-devbox-onbehalf.ps1
@@ -1,7 +1,7 @@
 # This is just a testing script in progress to create a dev box on behalf of the lab user.
 
 $resourceGroupName = 'onBehalfResourceGroup-buildlab'
-$location = 'EastUS'
+$location = 'WestUS3'
 $userID = '7e199eac-e561-43fc-b1d3-dd9dbb4bef71' # This is the object ID of the cloudslice-app
 
 # Ensure you are logged into your Azure account
@@ -14,8 +14,11 @@ if (-not $resourceGroup) {
 }
 
 # Create a devcenter for the user
-$devCenterName = 'onBehalfDevCenter'
-New-AzDevCenter -ResourceGroupName $resourceGroupName -Name $devCenterName -Location $location
+$devCenterName = 'onBehalfDevCenter' 
+ az devcenter admin devcenter create 
+    -Name $devCenterName
+    -ResourceGroupName $resourceGroupName 
+    -Location $location
 
 # Create a Dev Box definition
 $devBoxDefinitionName = 'VSCodeWin11WithHibernate-devboxdefinition'
@@ -25,7 +28,7 @@ $imageReference = @{
     Sku       = 'win11-21h2-pro'
     Version   = 'latest'
 }
-New-AzDevBoxDefinition -ResourceGroupName $resourceGroupName `
+    -ResourceGroupName $resourceGroupName `
     -DevCenterName $devCenterName `
     -Name $devBoxDefinitionName `
     -ImageReference $imageReference `
@@ -34,24 +37,24 @@ New-AzDevBoxDefinition -ResourceGroupName $resourceGroupName `
 
 # Add a project in the resource group
 $projectName = 'obBehalfProject'
-New-AzDevCenterProject -ResourceGroupName $resourceGroupName `
+    -ResourceGroupName $resourceGroupName `
     -DevCenterName $devCenterName `
     -Name $projectName `
     -Location $location
 
 # Create a new pool in the project using Microsoft-hosted network in WestUS3
 $poolName = 'onBehalfPool'
-New-AzDevCenterPool -ResourceGroupName $resourceGroupName `
+    -ResourceGroupName $resourceGroupName `
     -DevCenterName $devCenterName `
     -ProjectName $projectName `
     -Name $poolName `
     -DevBoxDefinitionName $devBoxDefinitionName `
     -NetworkConnectionType 'MicrosoftHostedNetwork' `
-    -Location 'WestUS3'
+    -Location $location
 
 # Create a new dev box in the pool
 $devBoxName = 'onBehalfDevBox'
-New-AzDevCenterDevBox -ResourceGroupName $resourceGroupName `
+    -ResourceGroupName $resourceGroupName `
     -DevCenterName $devCenterName `
     -ProjectName $projectName `
     -PoolName $poolName `
