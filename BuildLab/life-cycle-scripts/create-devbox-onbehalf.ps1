ed2e06163834df868591a307d49988b5dfde9a18
Jyoti Lama <jyotilama@microsoft.com>
2025-04-29 22:58:24 -0700

    clean up

--- BuildLab/life-cycle-scripts/create-devbox-onbehalf.ps1
+++ BuildLab/life-cycle-scripts/create-devbox-onbehalf.ps1
@@ -3,14 +3,16 @@
 # Don't need az login and the devcenter extension has already been installed on the vm disk.
 
 # Variables
-$resourceGroupName = 'onbehalfrg'
-$devCenterName = 'onBehalfDevCenter'
+$resourceGroupName = 'Build-2025'
 $location = 'westus3'
-$userID = '7e199eac-e561-43fc-b1d3-dd9dbb4bef71' # This is the object ID of the cloudslice-app
 
-# test create a devcenter
-az group create -l $location -n $resourceGroupName
-az devcenter admin devcenter create --location $locaiton --name $devCenterName --resource-group $resourceGroupName
+# Every instance of the lab vm will generate a new subscription ID, which will make the of the dev center name unique.
+$subId = az account show --query "{SubscriptionId:id}" --output tsv
+$devCenterName = 'build-' + $subId.Substring(0, 6) + "-dc"
+
+# TEST create a devcenter
+#az group create -l $location -n $resourceGroupName
+#az devcenter admin devcenter create --location $locaiton --name $devCenterName --resource-group $resourceGroupName
 
 # Create a log analytics workspace for the dev center
 $laworkspace = az monitor log-analytics workspace create --resource-group $resourceGroupName --workspace-name "DevCenterLogs" --location "westus2"
@@ -23,9 +25,35 @@ $devcenterid = $devcenters.id
 az monitor diagnostic-settings create --name DevCenter-Diagnostics --resource $devcenterid --logs '[{"categoryGroup":"allLogs","enabled":true}]' --workspace $laworkspaceid
 
 
-# Create a new dev box in the pool
+# Create a new dev box
 $projectName = "myProject"
 $poolName = "basic-image-pool"
-$devBoxName = "myDevBox"
-$userID = '7e199eac-e561-43fc-b1d3-dd9dbb4bef71' # This is the object ID of the cloudslice-app
-az devcenter dev dev-box create --pool-name $poolName --name $devBoxName --dev-center-name $devCenterName --project-name $projectName --user-id $userID
+$userID = '69d563db-e4f3-4bd3-be8c-44926ea56a7d' # This is the object ID of the cloud-slice-app
+
+# Option 1: Create dev box using az devcenter command
+#az devcenter dev dev-box create --pool-name $poolName --name $devBoxName --dev-center-name $devCenterName --project-name $projectName --user-id $userID
+
+# Option 2: Send request to create dev box
+$tenantId = "4cfe372a-37a4-44f8-91b2-5faf34253c62" # This is the tenat ID of the cloudslice-app
+#$devboxLocation = "centraluseuap" # TESIING in eueap
+
+# Create the request body
+$requestBody = @{
+    poolName = $poolName
+    osType = "Windows"
+}
+
+# Get the Azure AD token
+$token = az account get-access-token --resource 'https://devcenter.azure.com' --query accessToken --output tsv
+
+# Convert the request body to JSON
+$jsonBody = $requestBody | ConvertTo-Json
+
+# Define the API endpoint
+$apiUrl = "https://$tenantId-$devcenterName.$devboxLocation.devcenter.azure.com/projects/$projectName/users/$userID/devboxes/my-build-devbox?api-version=2025-02-01"
+
+# Send the web request to create the Dev Box
+$response = Invoke-RestMethod -Uri $apiUrl -Method Put -Headers @{Authorization = "Bearer $token"} -Body $jsonBody -ContentType "application/json"
+
+# Output the response
+$response
