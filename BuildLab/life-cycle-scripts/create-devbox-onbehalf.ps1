b10bee743c30ff0908fb06255fa357354be469bb
Jyoti Lama <jyotilama@microsoft.com>
2025-05-01 10:09:09 -0700

    revert to write error

--- BuildLab/life-cycle-scripts/create-devbox-onbehalf.ps1
+++ BuildLab/life-cycle-scripts/create-devbox-onbehalf.ps1
@@ -2,7 +2,7 @@
 
 # Don't need az login and the devcenter extension has already been installed on the vm disk.
 
-Write-Host "Check Sync: Hello world - test log"
+Write-Error "Check Sync: Hello world - test log"
 
 # Variables
 $resourceGroupName = 'Build-2025'
@@ -12,7 +12,7 @@ $location = 'westus3'
 $subId = az account show --query "{SubscriptionId:id}" --output tsv
 $devCenterName = "build-$($subId.SubString(0,6))-dc"
 
-Write-Host "attempting to add monitoring to the devcenter"
+Write-Error "attempting to add monitoring to the devcenter"
 
 # Create a log analytics workspace for the dev center
 $laworkspace = az monitor log-analytics workspace create --resource-group $resourceGroupName --workspace-name "DevCenterLogs" --location "westus2"
@@ -24,13 +24,13 @@ $rgComponent = "/resourceGroups/$resourceGroupName"
 $providerComponent = "/providers/Microsoft.DevCenter/devcenters/$devCenterName"
 $devcenterid = "$subscriptionComponent$rgComponent$providerComponent"
 
-Write-Host "got devcenter id: $devcenterid"
+Write-Error "got devcenter id: $devcenterid"
 
 az monitor diagnostic-settings create --name DevCenter-Diagnostics --resource $devcenterid --logs '[{"categoryGroup":"allLogs","enabled":true}]' --workspace $laworkspaceid
 
-Write-Host "monitoring added to the devcenter"
+Write-Error "monitoring added to the devcenter"
 
-Write-Host "attempting to create dev box on behalf of the user"
+Write-Error "attempting to create dev box on behalf of the user"
 
 # Create a new dev box
 $projectName = "myProject"
@@ -42,7 +42,7 @@ $userID = '69d563db-e4f3-4bd3-be8c-44926ea56a7d' # This is the object ID of the 
 
 # Option 2: Send request to create dev box
 $tenantId = "4cfe372a-37a4-44f8-91b2-5faf34253c62" # This is the tenat ID of the cloudslice-app
-#$devboxLocation = "centraluseuap" # TESIING in eueap
+$devboxLocation = "centraluseuap" # TESIING in eueap
 
 # Create the request body
 $requestBody = @{
@@ -50,12 +50,12 @@ $requestBody = @{
     osType = "Windows"
 }
 
-Write-Host "fetch token"
+Write-Error "fetch token"
 
 # Get the Azure AD token
 $token = az account get-access-token --resource 'https://devcenter.azure.com' --query accessToken --output tsv
 
-Write-Host "fetch token complete"
+Write-Error "fetch token complete"
 
 # Convert the request body to JSON
 $jsonBody = $requestBody | ConvertTo-Json
@@ -63,13 +63,13 @@ $jsonBody = $requestBody | ConvertTo-Json
 # Define the API endpoint
 $apiUrl = "https://$tenantId-$devcenterName.$devboxLocation.devcenter.azure.com/projects/$projectName/users/$userID/devboxes/my-build-devbox?api-version=2025-04-01-preview"
 
-Write-Host "send request to create dev box"
-Write-Host "API URL: $apiUrl"
+Write-Error "send request to create dev box"
+Write-Error "API URL: $apiUrl"
 
 # Send the web request to create the Dev Box
 $response = Invoke-RestMethod -Uri $apiUrl -Method Put -Headers @{Authorization = "Bearer $token"} -Body $jsonBody -ContentType "application/json"
 
-Write-Host "Request sent"
+Write-Error "Request sent"
 
 # Output the response
 $response
